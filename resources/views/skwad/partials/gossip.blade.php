0c3b7d66096e83e365eac6e850ea6805fb625e99
Paul Fanone <pfanone@gmail.com>
2017-06-07 12:03:35 -0400

    Update

--- resources/views/skwad/partials/gossip.blade.php
+++ resources/views/skwad/partials/gossip.blade.php
@@ -2,6 +2,6 @@
 	<div class="col-xs-12 col-sm-5">
 		<img class="img-responsive img-thumbnail" src="{{ $image_url }}">
 	</div>
-	<div class="col-xs-12 col-sm-7 padR30">{!! $description !!}</div>
-	<div class="col-xs-12 text-right">{{ $posted_date }}</div>
+	<div class="col-xs-12 col-sm-7">{!! $description !!}</div>
+	<div class="col-xs-12 text-right">Posted: {{ $posted_date }}</div>
 </div>

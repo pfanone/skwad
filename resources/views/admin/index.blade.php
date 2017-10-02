e6cd2cafd49f9e14929cded92b49b30bba185ff5
Paul Fanone <pfanone@gmail.com>
2017-10-02 13:50:09 -0400

    Updates

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,17 +11,25 @@
 		<div class="col-xs-12">
 			<form role="form" method="POST" action="{{ url('/admin') }}">
 				{{ csrf_field() }}
-				<div class="row">
-					<div class="col-xs-12">
+					<div class="form-group">
+						<label for="item_type">Description</label>
 						<select name="item_type">
 							<option value='news'>News</option>
 							<option value='rotisserie'>Rotisserie</option>
 							<option value='mission'>Mission</option>
 						</select>
 					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-12">
+					<div class="form-group">
+						<label for="item_header">Title</label>
+						<input type="text" name="item_header" class="form-control">
+					</div>
+					<div class="form-group">
+						<label for="item_description">Description</label>
+						<textarea name="item_description"></textarea>
+					</div>
+					<div class="col-xs-12 marginT10">
+					</div>
+					<div class="col-xs-12 marginT10">
 						<button type="submit">Submit</button>
 					</div>
 				</div>

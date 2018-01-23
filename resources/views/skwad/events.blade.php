06879b71b72e1629490b0f53021996011b9166f2
Paul Fanone <pfanone@gmail.com>
2018-01-23 12:27:13 -0500

    Fix

--- resources/views/skwad/events.blade.php
+++ resources/views/skwad/events.blade.php
@@ -5,12 +5,16 @@
 
 <div class="container">
 	<div class="row"> 
-		@if (isset($gossip['gossip']))
-		@foreach($gossip['gossip'] as $key => $value)
+		@if (isset($events['gossip']))
+			@foreach($events['gossip'] as $key => $value)
+				<div class="col-xs-12">
+					@include('skwad.partials.gossip', $value)
+				</div>
+			@endforeach
+		@else
 			<div class="col-xs-12">
-				@include('skwad.partials.gossip', $value)
+				<p>No Events!</p>
 			</div>
-		@endforeach
 		@endif
 	</div>
 </div>
